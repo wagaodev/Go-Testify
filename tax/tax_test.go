85b4cd5003fe13d0934d8502b72e60efbe94fd7e
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-28 03:08:12 -0300

    test: test calculate tax

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -1,6 +1,7 @@
 package tax
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,5 +15,17 @@ func TestCalculateTax(t *testing.T) {
 	tax, err = CalculateTax(0)
 	assert.Error(t, err, "amount must be greater than 0")
 	assert.Equal(t, 0.0, tax)
+}
 
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
+
+	repository.AssertExpectations(t)
 }
